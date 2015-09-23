d3b88bb921a7a084afe23088c78dddd776e9c326
JasonPan <jason8399@gmail.com>
2015-09-23 11:44:10 +0800

    Complete Activity2.java.

--- Lab1/app/src/main/java/jasonpan/lab1/Activity2.java
+++ Lab1/app/src/main/java/jasonpan/lab1/Activity2.java
@@ -2,12 +2,31 @@ package jasonpan.lab1;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Activity2 extends ActionBarActivity {
+
+    private Button Finish;
+    private TextView string;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_activity2);
+
+        Finish = (Button) findViewById(R.id.button2);
+        string = (TextView) findViewById(R.id.textView4);
+
+        Bundle bundle;
+        bundle = this.getIntent().getExtras();
+        string.setText(bundle.getString("KEY_TEXT"));
+        Finish.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
