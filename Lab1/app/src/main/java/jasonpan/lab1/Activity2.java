a281ed83e6a7a9c3d9948b116cfc88206d4189c3
JasonPan <jason8399@gmail.com>
2015-09-23 12:40:26 +0800

    add TextSize increase, decrease.

--- Lab1/app/src/main/java/jasonpan/lab1/Activity2.java
+++ Lab1/app/src/main/java/jasonpan/lab1/Activity2.java
@@ -2,14 +2,19 @@ package jasonpan.lab1;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.TypedValue;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
 
 public class Activity2 extends ActionBarActivity {
 
     private Button Finish;
     private TextView string;
+    private Button TextSizeInc;
+    private Button TextSizeDec;
+    private float TextSize;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,14 +23,33 @@ public class Activity2 extends ActionBarActivity {
 
         Finish = (Button) findViewById(R.id.button2);
         string = (TextView) findViewById(R.id.textView4);
+        TextSizeInc = (Button) findViewById(R.id.button3);
+        TextSizeDec = (Button) findViewById(R.id.button4);
 
         Bundle bundle;
         bundle = this.getIntent().getExtras();
         string.setText(bundle.getString("KEY_TEXT"));
+        TextSize = string.getTextSize();
         Finish.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 finish();
+            }
+        });
+
+        TextSizeInc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (TextSize <= 300)
+                    string.setTextSize(TypedValue.COMPLEX_UNIT_PX, TextSize += 10);
+            }
+        });
+
+        TextSizeDec.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(TextSize > 0)
+                    string.setTextSize(TypedValue.COMPLEX_UNIT_PX, TextSize -= 10);
             }
         });
     }
