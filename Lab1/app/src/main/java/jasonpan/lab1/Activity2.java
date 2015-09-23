3e013ead4819b84bc17f2bd1865362717767fcd5
JasonPan <jason8399@gmail.com>
2015-09-23 11:17:57 +0800

    Finish MainActivity, add Activity2.

---
+++ Lab1/app/src/main/java/jasonpan/lab1/Activity2.java
@@ -0,0 +1,13 @@
+package jasonpan.lab1;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+public class Activity2 extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_activity2);
+    }
+}
--- Lab1/app/src/main/java/jasonpan/lab1/MainActivity.java
+++ Lab1/app/src/main/java/jasonpan/lab1/MainActivity.java
@@ -1,13 +1,35 @@
 package jasonpan.lab1;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends ActionBarActivity {
+
+    private Button Switch;
+    private EditText edit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Switch = (Button) findViewById(R.id.button);
+        edit = (EditText) findViewById(R.id.editText);
+
+        Switch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(MainActivity.this, Activity2.class);
+                Bundle bundle = new Bundle();
+                bundle.putString("KEY_TEXT", edit.getText().toString());
+                intent.putExtras(bundle);
+                startActivity(intent);
+            }
+        });
     }
 }
