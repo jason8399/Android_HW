17c794b1ccf58c7030f8596419954a31e211ee6d
JasonPan <jason8399@gmail.com>
2015-12-04 12:05:59 +0800

    Lab5 init.

---
+++ Lab5/app/src/main/java/jason/lab/lab5/MainActivity.java
@@ -0,0 +1,49 @@
+package jason.lab.lab5;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.WebView;
+
+public class MainActivity extends Activity {
+	
+	private static final String MAP_URL = "file:///android_asset/Map.html";
+	private WebView webView;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		setupWebView();
+		this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+		
+	}
+	
+	@SuppressLint("SetJavaScriptEnabled") private void setupWebView(){
+		webView = (WebView) findViewById(R.id.webView1);
+		webView.getSettings().setJavaScriptEnabled(true);
+		webView.loadUrl(MAP_URL);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}
